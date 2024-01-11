159ec6a0ac24dd06d494d855e9cb45c48935635b
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-11 11:48:25 +0400

    fixing height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_heighti = 0, right_height = 0;
+	size_t left_height = 0, right_height = 0;
 
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
